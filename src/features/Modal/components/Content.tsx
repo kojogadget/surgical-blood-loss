a9064b5589e27f4f7fef19a1b0e7141b37c5ad9e
Kjartan Gundersen <kjartan@gunder.dev>
2024-01-29 10:58:24 +0100

    Fix typo in modal condition

--- src/features/Modal/components/Content.tsx
+++ src/features/Modal/components/Content.tsx
@@ -160,7 +160,7 @@ export default function Content({
                                             : 'Ikke oppgitt'}
                                     </td>
                                 </tr>
-                                <tr className="border-b border-gray-200 bg-gray-50">
+                                <tr className="border-b border-gray-200">
                                     <td className="max-w-0 py-5 pl-4 pr-3 text-left text-sm sm:pl-0">
                                         Vekt:
                                     </td>
@@ -170,12 +170,12 @@ export default function Content({
                                             : 'Ikke oppgitt'}
                                     </td>
                                 </tr>
-                                <tr className="border-b border-gray-200 bg-gray-50">
+                                <tr className="border-b border-gray-200">
                                     <td className="max-w-0 py-5 pl-4 pr-3 text-left text-sm sm:pl-0">
                                         Blodtap:
                                     </td>
                                     <td className="py-5 pl-3 pr-4 text-right text-sm text-gray-500 sm:pr-0">
-                                        {data.bloodLoss <= 0
+                                        {data.bloodLoss >= 0
                                             ? data.bloodLoss
                                             : 'UGYLDIG VERDIER'}
                                     </td>
